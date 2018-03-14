b06abb38ebd6afec9fee2f94e48580784e4ba7be
Ryan Petschek <petschekr@gmail.com>
2018-03-14 12:48:50 -0400

    Add accuracy to weapons

--- js/weapons.ts
+++ js/weapons.ts
@@ -14,6 +14,7 @@ export interface Weapon {
 	//acrossGroundSpeed: number; // Instant for now, ignore arcing
 	efficacy: Map<UnitType, number>,
 	fireRate: number; // Rounds per minute
+	accuracy: number; // 0 to 1
 	// Effect of 0 = point (bullet, sabot, other projectiles)
 	terminalEffect: number; // Meters
 }
@@ -24,48 +25,52 @@ export namespace Weapons {
 		name: "AK-74",
 		range: 300,
 		efficacy: new Map([
-			[UnitType.Infantry, 1],
-			[UnitType.UnarmoredVehicle, 0.8],
-			[UnitType.LightArmor, 0.05],
-			[UnitType.HeavyArmor, 0.01]
+			[UnitType.Infantry, 20],
+			[UnitType.UnarmoredVehicle, 15],
+			[UnitType.LightArmor, 0.5],
+			[UnitType.HeavyArmor, 0.1]
 		]),
 		fireRate: 100,
+		accuracy: 0.3,
 		terminalEffect: 0
 	};
 	export const DANA: Weapon = {
 		name: "152mm SpGH DANA",
 		range: 18000,
 		efficacy: new Map([
-			[UnitType.Infantry, 1],
-			[UnitType.UnarmoredVehicle, 1],
-			[UnitType.LightArmor, 0.5],
-			[UnitType.HeavyArmor, 0.5]
+			[UnitType.Infantry, 100],
+			[UnitType.UnarmoredVehicle, 100],
+			[UnitType.LightArmor, 50],
+			[UnitType.HeavyArmor, 45]
 		]),
 		fireRate: 3,
+		accuracy: 0.2,
 		terminalEffect: 75
 	};
 	export const D10: Weapon = {
 		name: "T-55 D-10",
 		range: 1400,
 		efficacy: new Map([
-			[UnitType.Infantry, 0.9],
-			[UnitType.UnarmoredVehicle, 1],
-			[UnitType.LightArmor, 1],
-			[UnitType.HeavyArmor, 1]
+			[UnitType.Infantry, 90],
+			[UnitType.UnarmoredVehicle, 100],
+			[UnitType.LightArmor, 100],
+			[UnitType.HeavyArmor, 80]
 		]),
 		fireRate: 4,
+		accuracy: 0.7,
 		terminalEffect: 10
 	};
 	export const SGMT: Weapon = {
 		name: "T-55 SGMT",
 		range: 1000,
 		efficacy: new Map([
-			[UnitType.Infantry, 1],
-			[UnitType.UnarmoredVehicle, 0.95],
-			[UnitType.LightArmor, 0.05],
-			[UnitType.HeavyArmor, 0.05]
+			[UnitType.Infantry, 40],
+			[UnitType.UnarmoredVehicle, 30],
+			[UnitType.LightArmor, 0.5],
+			[UnitType.HeavyArmor, 0.5]
 		]),
 		fireRate: 600,
+		accuracy: 0.4,
 		terminalEffect: 0
 	}
 }
